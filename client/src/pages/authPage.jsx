e3de9d0dcf7e5d27821d16d41a1a501b3d4aee6d
nawavar <hujjatx@yahoo.com>
2024-08-15 05:21:51 +0430

    logout button added, poppins font added

--- client/src/pages/authPage.jsx
+++ client/src/pages/authPage.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import axios from "axios";
-import SignUp from "./SignUp";
 import { Link } from "react-router-dom";
 
 const AuthPage = (props) => {
--- client/src/pages/chatsPage.jsx
+++ client/src/pages/chatsPage.jsx
@@ -1,20 +1,21 @@
 import { PrettyChatWindow } from "react-chat-engine-pretty";
+import { IoIosLogOut } from "react-icons/io";
 
 const ChatsPage = (props) => {
   return (
-    <div style={{ height: "100vh", width: "100vw" }}>
+    <div style={{ height: "100vh", width: "100vw", backgroundColor: "white" }}>
       <PrettyChatWindow
         projectId={process.env.REACT_APP_CHAT_ENGINE_PROJECT_ID}
         username={props.user.username}
         secret={props.user.secret}
-        style={{ height: "100%" }}
+        style={{ height: "100%", backgroundColor: "white" }}
       />
-      <button
+      <IoIosLogOut
         onClick={props.onLogout}
         style={{ position: "absolute", top: 10, right: 10 }}
-      >
-        Logout
-      </button>
+        color="white"
+        size={24}
+      />
     </div>
   );
 };
